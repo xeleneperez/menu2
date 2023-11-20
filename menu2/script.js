4bbbf9dc8aa08dc858cb3f2af051f797276340f9
xeleneperez <125167967+xeleneperez@users.noreply.github.com>
2023-11-19 20:10:47 -0500

    Add files via upload

---
+++ menu2/script.js
@@ -0,0 +1,13 @@
+document.querySelector(".menuBarra").addEventListener("click",animacionMenu);
+
+let barra1= document.querySelector(".fila1");
+let barra2= document.querySelector(".fila2");
+let barra3= document.querySelector(".fila3");
+//activa lo estilos que creamos en el css 
+
+
+function animacionMenu(){
+    barra1.classList.toggle("fila1Animacion");
+    barra3.classList.toggle("fila3Animacion");
+    barra2.classList.toggle("fila2Animacion");
+}
